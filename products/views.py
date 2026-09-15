d08b83d34011e75b871024a2e510a61aa8d04704
zehrademirc <demiricz.29@gmail.com>
2026-09-15 16:14:14 +0300

    Yeni kategori ve ürün şablonları eklendi.

--- products/views.py
+++ products/views.py
@@ -4,24 +4,35 @@ from .models import Product,Category
 
 # Create your views here.
 
-def home(request,category_slug=None):
+def home(request, category_slug=None):
     kategoriler = Category.objects.all()
 
     if category_slug:
-        secilen_kategori = get_object_or_404(Category,slug=category_slug)
+        secilen_kategori = get_object_or_404(Category, slug=category_slug)
         urunler = Product.objects.filter(category=secilen_kategori)
+
+        context = {
+            'categories': kategoriler,
+            'products': urunler,
+            'secilen_kategori': secilen_kategori
+        }
+
+        return render(request, 'kategori.html', context)
+
     else:
         urunler = Product.objects.all()
-        secilen_kategori = None
 
-    context = {
-        'categories': kategoriler,
-        'products': urunler,
-        'secilen_kategori': secilen_kategori
-    }
-    return render(request, 'anasayfa.html', context)
+        context = {
+            'categories': kategoriler,
+            'products': urunler,
+            'secilen_kategori': None
+        }
 
-def product_detail(request,id):
-    urun = get_object_or_404(Product,id=id)
+        return render(request, 'anasayfa.html', context)
+#bu değişikliği z yaptı!
+def product_detail(request, id):
+    print("PRODUCT DETAIL ÇALIŞTI:", id)
 
-    return render(request,'products/product_detail.html' ,{'product':urun})
+    urun = get_object_or_404(Product, id=id)
+
+    return render(request, 'products/product_detail.html', {'product': urun})
